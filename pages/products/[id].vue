760ddb99787bf6dd373876977c2431a6bb192218
jcbcouch <jcbcouch@gmail.com>
2022-11-20 19:31:18 -0600

    links and templates

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Voluptates deleniti rem laboriosam aut minus officiis quibusdam eos optio! Natus, ex!</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+definePageMeta({
+    layout: 'products'
+})
+</script>
+<style scoped>
+
+</style>
